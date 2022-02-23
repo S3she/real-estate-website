701b11e92e4e7b61b1c39d385485268f107c2e36
Isabella <90781622+S3she@users.noreply.github.com>
2022-02-23 20:52:30 +0100

    footer update

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
 
+import React from "react";
+
+
+
+import Footer from "./compontents js/Footer";
+
+import "./App.css";
+
+const App = () => (
+    <div className="App"> 
+    <h3></h3>
+    <h2></h2>
+    
+    <Footer />
+    </div>
+)
+
+export default App;

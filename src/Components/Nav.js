8923c8cf37dfb106c86589e7e768cd15137e3da3
MsDgoon <92296157+MsDgoon@users.noreply.github.com>
2022-02-28 13:53:05 +0100

    Navbar small uppdate-logo

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -10,13 +10,13 @@ const Nav = () => {
 
     return (
         <nav>
-            <h3>Logo</h3>
+            <><img src={require('../images/logga_g.png')} alt="logo" /></>
             <ul className="nav-links-style">
             <li><Link style={navStyle} to="/">Home</Link></li>
             <li><Link style={navStyle} to="/About">About Us</Link></li>
             <li><Link style={navStyle} to="/Sale">Homes for sale</Link></li>
             <li><Link style={navStyle} to="/Services">Our Services</Link></li>
-            <li><Link style={navStyle} to="/Blog">Contact</Link></li>
+            <li><Link style={navStyle} to="/Blog">Blog</Link></li>
                 <Link style={navStyle} to="/Booking">
                     <li>Booking</li>
                 </Link>

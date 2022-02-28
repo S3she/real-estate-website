a0560337885b17475528d01fd58477cd87d0e9d7
MsDgoon <92296157+MsDgoon@users.noreply.github.com>
2022-02-28 16:47:06 +0100

    Uppdaterad meny

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
-
+import bootstrap from 'bootstrap';
+import "./Nav.css";
 
 const Nav = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
 
     const navStyle = {
         color: 'white'
@@ -12,15 +14,34 @@ const Nav = () => {
         <nav>
             <><img src={require('../images/logga_g.png')} alt="logo" /></>
             <ul className="nav-links-style">
-            <li><Link style={navStyle} to="/">Home</Link></li>
-            <li><Link style={navStyle} to="/About">About Us</Link></li>
-            <li><Link style={navStyle} to="/Sale">Homes for sale</Link></li>
-            <li><Link style={navStyle} to="/Services">Our Services</Link></li>
-            <li><Link style={navStyle} to="/Blog">Blog</Link></li>
-                <Link style={navStyle} to="/Booking">
-                    <li>Booking</li>
-                </Link>
+                <li><Link style={navStyle} to="/">Home</Link></li>
+                <li><Link style={navStyle} to="/About">About Us</Link></li>
+                <li><Link style={navStyle} to="/Sale">Homes for sale</Link></li>
+                <li><Link style={navStyle} to="/Services">Our Services</Link></li>
+                <li><Link style={navStyle} to="/Blog">Blog</Link></li>
+                <li><Link style={navStyle} to="/Booking">Booking</Link></li>
+            <button type="button" className="new_button">Contact</button>       
             </ul>
+
+        {/* <div> */}
+        <div className="nav-small-screen">
+            <i class="bi bi-list" fontSize={27} onClick={() => setToggleMenu(true)}></i>
+            </div>
+            
+            {toggleMenu && (
+            <div className="nav-small-screen-overlay">
+                <i class="bi bi-x" fontSize={27} alt="close" onClick={() => setToggleMenu(false)}></i>
+            <ul className="nav-small-links">
+                <li><Link style={navStyle} to="/">Home</Link></li>
+                <li><Link style={navStyle} to="/About">About Us</Link></li>
+                <li><Link style={navStyle} to="/Sale">Homes for sale</Link></li>
+                <li><Link style={navStyle} to="/Services">Our Services</Link></li>
+                <li><Link style={navStyle} to="/Blog">Blog</Link></li>
+                <li><Link style={navStyle} to="/Booking">Booking</Link></li>
+            <button type="button" className="button_contact">Contact</button>       
+            </ul>
+            </div>
+            )}
         </nav>
     );
 }

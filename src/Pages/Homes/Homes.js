15a009e842adddd5bfb3bd0717f46c1940191a57
MsDgoon <92296157+MsDgoon@users.noreply.github.com>
2022-03-01 14:35:09 +0100

    upp

--- src/Pages/Homes/Homes.js
+++ src/Pages/Homes/Homes.js
@@ -1,16 +1,33 @@
 import React from "react";
 import './Homes.css';
-//import ReusableButton from "./ReusableButton";
+import commercialImage from './jimmy-dean-Qngdf0kgGB4-unsplash.jpg'
 
 const Homes = () => {
-    return (
-       <>
-        <h1>Booking</h1>
-        <p>some text</p>
-        {/* <ReusableButton>About Page Button</ReusableButton>
-        <Button/> */}
-       </>
-    );
-};
 
+    const imageStyle = {
+    };
+
+  return (
+    <div className="container">
+        <div className="homes-img">
+        <img style={imageStyle} src={commercialImage}/>
+        </div>
+        <div className="hero-text">        
+            <div className="headline">
+                REAL ESTATE GOTHENBURG
+            </div>
+            <div className="text-paragraph">
+                På Real Estate möts du av mäklare som är serviceinriktade, 
+                målinriktade och extremt drivna i yrket. Vi jobbar individuellt efter var 
+                kunds specifika krav och vi är inte nöjda förrän du är nöjd.
+            </div>
+        </div>
+        
+    </div>
+  )
+}
 export default Homes;
+
+
+
+

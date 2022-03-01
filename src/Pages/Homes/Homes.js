135b9bc84db0cfe31cbecb54c4345d3d8a3caed7
Isabella <90781622+S3she@users.noreply.github.com>
2022-03-01 22:15:56 +0100

    bigger updates

--- src/Pages/Homes/Homes.js
+++ src/Pages/Homes/Homes.js
@@ -1,22 +1,41 @@
 import React from "react";
 import './Homes.css';
-// import commercialImage from './jimmy-dean-Qngdf0kgGB4-unsplash.jpg'
-
+import image from './ralph-ravi-kayden-2d4lAQAlbDA-unsplash.jpg'
+import imagetwo from './house-isolated-field.jpg'
+import imagetree from './todd-kent-178j8tJrNlc-unsplash.jpg'
+import imagefour from './3d-rendering-large-modern-contemporary-house-wood-concrete.jpg'
 const Homes = () => {
+
+    const imagePosition = {
+        width: '430px',
+        marginLeft: '7rem'
+        
+    };
 
 
   return (
     <div className="box">
-       
-        <div className="hero-text">        
+        
+
+              
+      
+        <div className="hero-text">     
+        
+ 
+
+        
             <div className="headline">
-               
+
+            <img style={imagePosition} src={image}/>
+            <img style={imagePosition} src={imagetwo}/>
+            <img style={imagePosition} src={imagetree}/>
+            <img style={imagePosition} src={imagefour}/>
+            
             </div>
-            <div className="text-paragraph">
-                
+            <div className="vitRektangel">
 
+               
 
-                HOMES FOR SALE
 
             </div>
         </div>
